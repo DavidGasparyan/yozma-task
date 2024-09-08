f4d7dcd69e9d0df781e1cb35f98714e9a5b514ec
David Gasparyan <davidgasparyang@gmail.com>
2024-09-08 21:38:46 +0400

    feat: configure cors for Angular and s declare typeorm

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,46 @@
+import 'reflect-metadata';
 import express from 'express';
-import Router from './config/routes';
+import bodyParser from 'body-parser';
+import { Pool } from 'pg';
+import routes from "./config/routes";
+import cors from "cors";
+import {AppDataSource} from "./data-source";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
-const port = 8080 || process.env.PORT;
+const port = 8080;
 
-app.use(Router);
+// Allow Angular development URL to access the application
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  // Use custom port
+  port: 5440,
+});
+
+app.set('pool', pool);
+app.use('/api', routes);
+
+app.use('/api', routes);
 
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+  console.log("Server is running on http://localhost:" + port);
 });
